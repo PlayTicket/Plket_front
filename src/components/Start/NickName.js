c65823172cc57811a39d4249b9623bd7ea172493
Seryeong <99seryoung50@gmail.com>
2021-10-27 22:07:04 +0900

    feat: StartMain & NickName

--- src/components/Start/NickName.js
+++ src/components/Start/NickName.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import {TouchableOpacity, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {TouchableOpacity, View, Text, TextInput} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import {COLORS} from '../../styles/Colors';
 
 const NickName = ({navigation}) => {
+  const [nickName, setNickName] = useState('');
+
+  AsyncStorage.setItem('NickName', nickName, () => {
+    console.log('유저 닉네임 저장 완료');
+  });
+
   return (
-    <View>
-      <TouchableOpacity onPress={() => navigation.navigate('Taste')}>
-        <Text>카카오톡으로 시작하기</Text>
+    <View
+      style={{
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '10%',
+        paddingBottom: '40%',
+      }}>
+      <Text style={{fontSize: 14, color: '#B6B6CC'}}>
+        저장하고 싶은 감동의 순간
+      </Text>
+      <Text style={{fontSize: 20, marginBottom: '1%'}}>
+        감상평을 기록해볼까요?
+      </Text>
+      <Text style={{fontSize: 18}}>닉네임 입력</Text>
+      <TextInput
+        style={{
+          borderBottomColor: '#000',
+          borderBottomWidth: 1,
+          marginTop: '20%',
+        }}
+        value={nickName}
+        onChange={event => {
+          const {eventCount, target, text} = event.nativeEvent;
+          setNickName(text);
+        }}
+      />
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Taste')}
+        style={{
+          marginTop: '15%',
+          backgroundColor: COLORS.navy,
+          height: '12%',
+          width: '100%',
+          borderRadius: 15,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{fontSize: 20, color: 'white'}}>시작하기</Text>
       </TouchableOpacity>
     </View>
   );
