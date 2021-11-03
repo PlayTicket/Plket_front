a46114ac8c0b62cd02e9f6e60a64fe4a1b57bb1f
Seryeong <99seryoung50@gmail.com>
2021-11-04 01:31:04 +0900

    feat: AddTicket

---
+++ src/components/AddTicket/AddStars.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const AddStars = () => {
+  return (
+    <View>
+      <View
+        style={{width: '100%', height: '70%', backgroundColor: 'gray'}}></View>
+    </View>
+  );
+};
+
+export default AddStars;
